7672f60f6789b212020e441ea4d439b820f7a3c1
Gaurav Sablok <sablokg@gmail.com>
2024-03-01 14:27:35 +0100

    Create estimate_similarity.py

---
+++ estimate_similarity.py
@@ -0,0 +1,29 @@
+def alignmentChecker(alignmentfile = None):
+  # Universitat Potsdam
+  # Author Gaurav Sablok
+  # date: 2024-3-1
+    """"
+    a site specific function to estimate the site similarity across the graphs
+    to make it faster, i implemented a linear approach so that it will compare 
+    the iterable as a list and then stores it in the another iterable. You can
+    nest these functions as callables under the class. 
+    """
+    if alignmentfile is not None:
+        readalignment = open(alignmentfile)
+        fastaidlist = []
+        fastasequences = []
+        for i in readalignment.readlines():
+            if i.startswith(">"):
+                fastaidlist.append(i.strip().replace(">", ""))
+            else:
+                fastasequences.append(i.strip())
+        resolveids = [j for i in fastaidlist for j in i]
+        resolvesequences = [j for i in fastasequences for j in i]
+    return list(zip(fastaidlist,fastasequences))
+def computeSimilarity(alignmentfile = None):
+    alignmentiterable = alignmentChecker(alignmentfile)
+    storingseq = list(map(lambda num: list(num[1]), alignmentiterable))
+    # [storingseq[i][j] for i in range(len(storingseq)-1) for j in storingseq[i] if storingseq[i][j] not in storingseq[i+1][j]] 
+    alignemnt_similarity = [storingseq[i] for i in range(len(storingseq)-1) \
+                                                 if storingseq[i] not in storingseq[i+1]]
+    return alignemnt_similarity
